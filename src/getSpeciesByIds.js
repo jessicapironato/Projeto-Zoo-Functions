db72612fdd36892b2a88f00b002be6e27b73431c
Jéssica <jessicapmaximo@gmail.com>
2022-12-08 15:31:15 -0300

    Requisito 1 ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  if (ids === undefined) {
+    return [];
+  }
+  const parametroDeclarado = ids.map((elemento) =>
+    species.find((specie) => specie.id === elemento));
+  return parametroDeclarado;
 };
-
+getSpeciesByIds();
+console.log(getSpeciesByIds());
+console.log(getSpeciesByIds('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
 module.exports = getSpeciesByIds;
