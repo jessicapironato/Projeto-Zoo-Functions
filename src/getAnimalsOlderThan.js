b6360ee68e11755aa787081ab96d3742dec6e609
Jéssica <jessicapmaximo@gmail.com>
2022-12-09 10:57:19 -0300

    getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,6 +1,9 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
+  const { residents } = species.find(({ name }) => name === animal);
+  return residents.every((element) => element.age >= age);
 };
 
+console.log(getAnimalsOlderThan('bears', '2'));
 module.exports = getAnimalsOlderThan;
