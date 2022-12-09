68d35f7370409cdbd607d88a3d7cb5e44e7db43b
Jéssica <jessicapmaximo@gmail.com>
2022-12-09 10:28:33 -0300

    requisito 5 ok

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, animalAtual) => {
+      acc[animalAtual.name] = animalAtual.residents.length;
+      return acc;
+    }, {});
+  }
+
+  const { residents } = species.find(({ name }) => name === animal.species);
+
+  if (animal.sex) {
+    const filter = residents.filter((resident) => resident.sex === animal.sex);
+    return filter.length;
+  }
+  return residents.length;
 };
+// console.log(countAnimals({ specie: 'penguins', sex: 'female' }));
 
 module.exports = countAnimals;
