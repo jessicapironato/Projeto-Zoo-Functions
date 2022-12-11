d24a631423c2fd43df9abeeecb5b1f4a41e2806f
Jéssica <jessicapmaximo@gmail.com>
2022-12-11 19:25:24 -0300

    getSchedule ok

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,6 +1,10 @@
-const data = require('../data/zoo_data');
+// const { species } = require('../data/zoo_data');
+// const { employees } = require('../data/zoo_data');
 
-const getOldestFromFirstSpecies = (id) => {
-};
+// // const getOldestFromFirstSpecies = (id) => {
+// //   const findAnimal = employees.find((animal) => animal.id === id).responsibleFor;
+// //   const findResponsabible = species.find((specie.id) = )
 
-module.exports = getOldestFromFirstSpecies;
+// // };
+
+// module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,55 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+const { hours } = require('../data/zoo_data');
+
+const getOfficeHours = (day) => {
+  if (day[1].open === 0 && day[1].close === 0) {
+    return 'CLOSED';
+  }
+  return `Open from ${day[1].open}am until ${day[1].close}pm`;
+};
+
+const getExhibition = (day) => {
+  if (day[1].open === 0 && day[1].close === 0) {
+    return 'The zoo will be closed!';
+  }
+  return species.filter((specie) =>
+    specie.availability.includes(day[0])).map(({ name }) => name);
+};
+
+const getSchedules = (dayofWeek) => {
+  if (dayofWeek === undefined) {
+    const all = {};
+    Object.entries(hours).forEach((day) => {
+      all[day[0]] = {
+        officeHour: getOfficeHours(day),
+        exhibition: getExhibition(day),
+      };
+    });
+    return all;
+  }
+  const day = Object.entries(hours).find((hour) => hour[0] === dayofWeek);
+  return {
+    [dayofWeek]: {
+      officeHour: getOfficeHours(day),
+      exhibition: getExhibition(day),
+    },
+  };
+};
 
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  if (scheduleTarget === undefined) {
+    return getSchedules();
+  }
+
+  if (hours[scheduleTarget] !== undefined) {
+    return getSchedules(scheduleTarget);
+  }
+
+  const specie = species.find((animal) => animal.name === scheduleTarget);
+  if (specie === undefined) {
+    return getSchedules();
+  }
+  return specie.availability;
 };
 
 module.exports = getSchedule;
