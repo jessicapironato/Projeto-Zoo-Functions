50c6b0baa975846dff7674f5f049ad319e14b408
Jéssica <jessicapmaximo@gmail.com>
2022-12-11 15:14:21 -0300

    calculateEntry ok

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,31 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 // iniciando projeto
+
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const counter = { adult: 0, child: 0, senior: 0 };
+  entrants.forEach(({ age }) => {
+    switch (true) {
+    case age < 18:
+      counter.child += 1;
+      break;
+    case age >= 18 && age < 50:
+      counter.adult += 1;
+      break;
+    default:
+      counter.senior += 1;
+    }
+  });
+  return counter;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const counter = countEntrants(entrants);
+
+  return counter.adult * prices.adult + counter.child * prices.child
+    + counter.senior * prices.senior;
 };
 
 module.exports = { calculateEntry, countEntrants };
